f1add69856d6fe092a366a70ab60131e5a8d6abd
natrix_dev <discordlenko@gmail.com>
2022-01-28 18:52:45 +0100

    Create Starter.py

---
+++ Starter.py
@@ -0,0 +1,31 @@
+import servers as .
+from port import *
+
+
+x = server.port("INSERT A SEVER PORT")
+y = .plugged-in("PLUGGED IN MESSAGE HERE")
+
+if y = true => {
+  print("Internet has been connected !" + .internet-connection-name)
+  print("Starter.py - By natrixdev" + port.github-folowers)
+  while 
+  print("Not connected")
+}
+  
+  
+ if x = true => {
+   print("Port " + x + "is " + $port_validity)
+   print("Starter.py - By natrixdev" + port.github-folowers)
+   while 
+   print("Not connected")
+ }
+
+from natrix import natrix as _
+# just for github stats, don't touche please 
+
+-github.export(number, sees, project-modifier, natrixdev, opens)
+-github.status(online, stars : true)
+   
+  
+  
+ 
